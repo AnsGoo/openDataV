0e92a318b8cb8481cbea849035f4f0081decd04d
chenghaiwen <haiven_123@163.com>
2022-02-16 09:50:34 +0800

    修正了预览

--- src/router/modules/basic.ts
+++ src/router/modules/basic.ts
@@ -94,7 +94,7 @@ const basicRoutes: AppRouteRecordRaw[] = [
   {
     path: '/:pathMatch(.*)*',
     name: 'Error',
-    component: () => import('@/views/exception/404.vue'),
+    component: () => import('@/views/designer/Index.vue'),
     meta: {
       title: '错误页面',
       ignoreAuth: true
