f733c2c22853886a98f6ec153a1269229b9bf9a9
chenghaiwen <haiven_123@163.com>
2022-08-29 21:52:20 +0800

    20220829

---
+++ assets/Image.bc899f66.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.3aefe4c7.js";import{g as _,o,v as t}from"./.pnpm.ec28eaac.js";const a={class:"bg"},s=_({__name:"Image",props:{component:null},setup(c){return(n,r)=>(o(),t("div",a))}});var l=e(s,[["__scopeId","data-v-fe45c0c4"]]);export{l as default};
--- assets/vue-demi.b3a9cad9.js
+++
@@ -1,1 +0,0 @@
-var a=!1;export{a as i};
