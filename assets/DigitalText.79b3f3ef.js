2040dc964cf3b839387cb3b60a1bd722866f838a
chenghaiwen <haiven_123@163.com>
2022-09-14 17:07:32 +0800

    20220914

---
+++ assets/DigitalText.79b3f3ef.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useProp.afe31ed3.js";import{d as s,i as p,o as c,c as r,t as u,u as i}from"./index.394616ab.js";const _=s({__name:"DigitalText",props:{component:null},setup(o){const t=o,{propValue:a}=n(t.component),e=p(()=>a.dataconfig.data);return(l,m)=>(c(),r("div",null,u(i(e)),1))}});export{_ as default};
