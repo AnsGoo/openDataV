cbcde8b903f08ff1f7ba65a15bd7b693d0ed5949
chenghaiwen <haiven_123@163.com>
2022-09-14 11:55:28 +0800

    20220915

---
+++ assets/404.ce1009dc.js
@@ -0,0 +1,1 @@
+import{d as t,Z as _,o as n,b as a,P as e,_ as r}from"./index.3fe0a683.js";const c=""+new URL("../errors/404.png",import.meta.url).href,p={class:"notfound"},m=t({__name:"404",setup(u){const o=_(),s=()=>{o.push({name:"Home"})};return(d,i)=>(n(),a("div",p,[e("div",null,[e("img",{src:c,onClick:s})])]))}});const f=r(m,[["__scopeId","data-v-27a57ee8"]]);export{f as default};
