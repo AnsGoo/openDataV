9a0d863a5c32ac9f3034509f727979e8aa743e2d
dengshangxing <sdeng@xadongda.com>
2022-09-22 18:09:33 +0800

    更新

---
+++ src/designer/Pane/RightSideBar/DataModule/DataList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>数据处理模块</div>
+</template>
+
+<script lang="ts" setup>
+import { BaseComponent } from '@/resource/models'
+
+defineProps<{
+  curComponent: BaseComponent
+}>()
+</script>
+
+<style lang="less" scoped></style>
