48cf8681c7f4349a5633127abc80c0b596c60c7f
chenghaiwen <haiven_123@163.com>
2022-09-23 10:33:36 +0800

    feat() 20220923

---
+++ assets/Image.d1de7519.js
@@ -0,0 +1,1 @@
+import{d as e,o as _,c,_ as o}from"./index.efbb82d5.js";const s={class:"bg"},t=e({__name:"Image",props:{component:null},setup(n){return(a,p)=>(_(),c("div",s))}});const m=o(t,[["__scopeId","data-v-8e421cc2"]]);export{m as default};
