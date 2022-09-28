e664beee68e33c4b4d00f54144f7b1cb90a9c783
chenghaiwen <haiven_123@163.com>
2022-09-28 22:32:28 +0800

    feat(data):hooks

--- src/resource/hooks/useData/index.ts
+++ src/resource/hooks/useData/index.ts
@@ -2,30 +2,29 @@ import { BaseComponent, DataType } from '@/resource/models'
 import { onMounted, onUnmounted } from 'vue'
 
 export const useData = (component: BaseComponent, callbackData?: (data: any) => void) => {
-  if (callbackData) {
+  if (callbackData && component.dataConfig) {
     component.changeDataCallback(callbackData)
-    let timer: IntervalHandle
-    onMounted(async () => {
-      const result = await component.requestConfig.getRespData()
+    let timer: IntervalHandle = 0
+    const pullData = async () => {
+      const result = await component.dataConfig?.requestConfig.getRespData()
       callbackData(result)
-    })
-
-    if (component.requestConfig.type === DataType.REST) {
-      if (timer) {
-        clearInterval(timer)
-      }
-
-      async function pullData() {
-        const result = await component.requestConfig.getRespData()
-        callbackData?(result)
-      }
-      const dataConfig = component.dataConfig
-      timer = setInterval(pullData, dataConfig.interval || 300)
     }
-    onUnmounted(() => {
-      if (timer) {
+    onMounted(async () => {
+      await pullData()
+    })
+
+    if (component.dataConfig?.type === DataType.REST && component.dataConfig.otherConfig.isRepeat) {
+      if (timer ) {
         clearInterval(timer)
       }
-    })
+      
+      const dataConfig = component.dataConfig
+      timer = setInterval(pullData, dataConfig.otherConfig.interval || 300)
+      onUnmounted(() => {
+        if (timer) {
+          clearInterval(timer)
+        }
+      })
+    }
   }
 }
