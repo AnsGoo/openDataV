e41d9005806ee7fced0685871836de0dd65a41dc
chenghaiwen <haiven_123@163.com>
2022-10-09 14:55:01 +0800

    202210096

---
+++ assets/Image.662060bf.js
@@ -0,0 +1,1 @@
+import{d as e,o as _,c,_ as o}from"./index.785a8cd0.js";const s={class:"bg"},t=e({__name:"Image",props:{component:null},setup(n){return(a,p)=>(_(),c("div",s))}});const m=o(t,[["__scopeId","data-v-8e421cc2"]]);export{m as default};
