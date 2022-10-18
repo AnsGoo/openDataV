ff17ad6b485f3da81e60c4acee60d1d220a74eec
qwecxzpoi <benyuanzhanggithub@outlook.com>
2022-10-18 18:59:16 +0800

    other(docs): 删除 docs 功能

--- docs/.vitepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig} from 'vitepress';
-
-export default defineConfig({
-  lang: 'zh-CN',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
-  appearance: true
-})
