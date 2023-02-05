0c854fdc24985facbcbe838bb83a430fe3df08ab
ansgoo <haiven_123@163.com>
2023-02-05 12:45:45 +0800

    feat(): 20230205

---
+++ assets/li.c08ef975.js
@@ -0,0 +1,5 @@
+import{ar as n,ap as i,aq as t,d as r,a3 as e}from"./index.642367b9.js";const a=n("li",{transition:"color .3s var(--n-bezier)",lineHeight:"var(--n-line-height)",margin:"var(--n-li-margin)",marginBottom:0,color:"var(--n-text-color)"}),o=[n("&:first-child",`
+ margin-top: 0;
+ `),n("&:last-child",`
+ margin-bottom: 0;
+ `)],s=n([i("ol",{fontSize:"var(--n-font-size)",padding:"var(--n-ol-padding)"},[t("align-text",{paddingLeft:0}),a,o]),i("ul",{fontSize:"var(--n-font-size)",padding:"var(--n-ul-padding)"},[t("align-text",{paddingLeft:0}),a,o])]),d=r({name:"Li",render(){return e("li",null,this.$slots)}});export{d as N,s};
