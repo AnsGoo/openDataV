f3441db289684ebcd1a35c7a92be4e9ca64e57de
ansgoo <haiven_123@163.com>
2023-02-05 12:32:38 +0800

    feat(): 20230205

---
+++ assets/Image.695df7e1.js
@@ -0,0 +1,1 @@
+import{d as e,o as _,c,_ as o}from"./index.b10b5c98.js";const s={class:"bg"},t=e({__name:"Image",props:{component:null},setup(n){return(a,p)=>(_(),c("div",s))}});const m=o(t,[["__scopeId","data-v-094bccc5"]]);export{m as default};
