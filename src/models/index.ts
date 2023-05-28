5d5069b52981df9a8e7b74c5d66db3a570bd7112
ansgoo <haiven_123@163.com>
2023-05-28 18:32:46 +0800

    fix: some import error

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,3 @@
 import { CustomComponent } from './component'
-import { DemoRequestData, RestRequestData, StaticRequestData } from './requestOption'
 
-export { CustomComponent, DemoRequestData, RestRequestData, StaticRequestData }
+export { CustomComponent }
