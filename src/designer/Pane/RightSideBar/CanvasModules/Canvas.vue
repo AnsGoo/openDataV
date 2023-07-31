bdb2a813f2a96c59baa3d46891c4e230e7fee740
ansgoo <haiven_123@163.com>
2023-07-31 23:14:08 +0800

    fix: 修复了分辨率和屏幕长宽高无法联动的异常

--- src/designer/Pane/RightSideBar/CanvasModules/Canvas.vue
+++ src/designer/Pane/RightSideBar/CanvasModules/Canvas.vue
@@ -1,19 +1,18 @@
 <template>
   <Container :config="config" :data="formData" :mode="mode" @change="changed" />
 </template>
-import Container from 'open-data-v/designer/modules/form/Container'
 <script setup lang="ts">
 import Container from 'open-data-v/designer/modules/form/Container'
 import useCanvasState from 'open-data-v/designer/state/canvas'
 import type { MetaContainerItem } from 'open-data-v/designer/type'
 import type { ContainerType } from 'open-data-v/enum'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 const canvasState = useCanvasState()
 const config = computed<Array<MetaContainerItem>>(() => canvasState.canvasStyleConfig.formItems!)
 const mode = computed<ContainerType>(() => canvasState.canvasStyleConfig.mode!)
 
-const formData = ref<Record<string, any>>(canvasState.globalOption)
+const formData = computed<Record<string, any>>(() => canvasState.globalOption)
 const changed = (keys: Array<string>, val: any) => {
   canvasState.setCanvasStyle(keys, val)
 }
