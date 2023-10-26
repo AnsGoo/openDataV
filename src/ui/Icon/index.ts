472f9cbfbf2177e5bd16f73c70f551caad6be0ac
ansgoo <haiven_123@163.com>
2023-10-26 21:10:42 +0800

    feat: 模态框增加了关闭按钮

--- src/ui/Icon/index.ts
+++ src/ui/Icon/index.ts
@@ -2,10 +2,21 @@ import Add from './Add'
 import ChevronDown from './ChevronDown'
 import CheveronLeft from './ChevronLeft'
 import CheveronRight from './ChevronRight'
+import Clear from './Clear'
 import Close from './Close'
 import Eye from './Eye'
 import EyeOff from './EyeOff'
 import Subtract from './Subtract'
 
-export { Add, CheveronLeft, CheveronRight, ChevronDown, Close, Eye, EyeOff, Subtract }
-export default { CheveronLeft, CheveronRight, Close, Eye, EyeOff, ChevronDown, Add, Subtract }
+export { Add, CheveronLeft, CheveronRight, ChevronDown, Clear, Close, Eye, EyeOff, Subtract }
+export default {
+  CheveronLeft,
+  CheveronRight,
+  Close,
+  Eye,
+  EyeOff,
+  ChevronDown,
+  Add,
+  Subtract,
+  Clear
+}
