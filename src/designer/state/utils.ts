462becf83d8c2ae7d2b550b2fb457dc5320be2e6
ansgoo <haiven_123@163.com>
2024-04-07 23:06:11 +0800

    refactor: state 更改为单例模式

---
+++ src/designer/state/utils.ts
@@ -0,0 +1,13 @@
+export type Class = { new (...args: any[]): any }
+
+export function singleton(className: Class): Class {
+  let ins: Class
+  return new Proxy(className, {
+    construct(target, ...args) {
+      if (!ins) {
+        ins = new target(...args)
+      }
+      return ins
+    }
+  })
+}
