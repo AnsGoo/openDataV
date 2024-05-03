d7f3b48aaafdb78c76b90a1052b4de12326d503a
ansgoo <haiven_123@163.com>
2024-05-03 13:07:22 +0800

    feat: 增加了示例

---
+++ resources/Basic/Image/panel.ts
@@ -0,0 +1,21 @@
+import { FormType } from '@open-data-v/base'
+
+export default {
+  style: [],
+  propValue: [
+    {
+      label: '背景设置',
+      prop: 'back',
+      children: [
+        {
+          prop: 'background',
+          label: '背景',
+          type: FormType.BACKGROUND,
+          props: {
+            defaultValue: { backgroundColor: '#14c9c9' }
+          }
+        }
+      ]
+    }
+  ]
+}
---
+++ resources/Chart/BasicBarChart/panel.ts
@@ -0,0 +1,215 @@
+import { FormType } from '@open-data-v/base'
+
+export default {
+  style: [],
+  propValue: [
+    {
+      label: '数据配置',
+      prop: 'data',
+      children: [
+        {
+          prop: 'upperLimit',
+          label: '上限',
+          type: FormType.NUMBER,
+          componentOptions: {
+            defaultValue: 150
+          }
+        },
+        {
+          prop: 'lowerLimit',
+          label: '下限',
+          type: FormType.NUMBER,
+          props: {
+            defaultValue: 0
+          }
+        },
+        {
+          prop: 'max',
+          label: '最大值',
+          type: FormType.TEXT,
+          props: {
+            defaultValue: 'dataMax'
+          }
+        },
+        {
+          prop: 'min',
+          label: '最小值',
+          type: FormType.TEXT,
+          props: {
+            defaultValue: '0'
+          }
+        },
+        {
+          prop: 'maxOffset',
+          label: '最大偏移值',
+          type: FormType.NUMBER,
+          props: {
+            defaultValue: 0
+          }
+        },
+        {
+          prop: 'minOffset',
+          label: '最小偏移值',
+          type: FormType.NUMBER,
+          props: {
+            defaultValue: 0
+          }
+        }
+      ]
+    },
+    {
+      label: '标签配置',
+      prop: 'label',
+      children: [
+        {
+          prop: 'axisLabelColor',
+          label: 'label颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#3DE7C9'
+          }
+        },
+        {
+          prop: 'axisColor',
+          label: '颜色1',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#3DE7C9'
+          }
+        }
+      ]
+    },
+    {
+      label: '坐标轴配置',
+      prop: 'axis',
+      children: [
+        {
+          prop: 'axisColor',
+          label: '轴线颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#00BAFF'
+          }
+        },
+        {
+          prop: 'axisLabelColor',
+          label: '轴线文字颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#00BAFF'
+          }
+        },
+        {
+          prop: 'xshow',
+          label: 'X网格线是否显示',
+          type: FormType.SWITCH,
+          props: {
+            defaultValue: true
+          }
+        },
+        {
+          prop: 'yshow',
+          label: 'Y网格线是否显示',
+          type: FormType.SWITCH,
+          props: {
+            defaultValue: true
+          }
+        },
+        {
+          prop: 'xLineType',
+          label: 'X轴网格线样式',
+          type: FormType.SELECT,
+          props: {
+            defaultValue: 'dotted',
+            options: [
+              { value: 'solid', label: 'solid' },
+              { value: 'dotted', label: 'dotted' },
+              { value: 'dashed', label: 'dashed' }
+            ]
+          }
+        },
+        {
+          prop: 'yLineType',
+          label: 'Y轴网格线样式',
+          type: FormType.SELECT,
+          props: {
+            defaultValue: 'dotted',
+            options: [
+              { value: 'solid', label: 'solid' },
+              { value: 'dotted', label: 'dotted' },
+              { value: 'dashed', label: 'dashed' }
+            ]
+          }
+        },
+        {
+          prop: 'xAxisLineColor',
+          label: 'x轴网格线颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#3391E4'
+          }
+        },
+        {
+          prop: 'yAxisLineColor',
+          label: 'y轴网格线颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#3391E4'
+          }
+        }
+      ]
+    },
+    {
+      label: '测试',
+      prop: 'test',
+      children: [
+        {
+          prop: 'axisColor',
+          label: '轴线颜色',
+          type: FormType.COLOR,
+          props: {
+            defaultValue: '#00BAFF'
+          }
+        },
+        {
+          prop: 'axisLabelColor',
+          label: '轴线文字颜色',
+          type: FormType.MODAL,
+          children: [
+            {
+              prop: 'axisColor23',
+              label: '轴线颜色',
+              type: FormType.COLOR,
+              props: {
+                defaultValue: '#00BAFF'
+              }
+            },
+            {
+              prop: 'axisLabelColor',
+              label: '颜色1',
+              type: FormType.COLOR,
+              props: {
+                defaultValue: '#3DE7C9'
+              }
+            }
+          ],
+          props: {
+            buttonText: '测试',
+            defaultValue: ''
+          }
+        }
+      ]
+    }
+  ],
+  demoLoader: () => {
+    return [
+      { label: '秦', value: Math.round(Math.random() * 100) },
+      { label: '齐', value: Math.round(Math.random() * 100) },
+      { label: '楚', value: Math.round(Math.random() * 100) },
+      { label: '赵', value: Math.round(Math.random() * 100) },
+      { label: '燕', value: Math.round(Math.random() * 100) },
+      { label: '韩', value: Math.round(Math.random() * 100) },
+      { label: '魏', value: Math.round(Math.random() * 100) }
+    ]
+  }
+}
