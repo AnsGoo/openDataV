1caf7c91492dcc8d0f06e7a2eb300e3598b5d917
ansgoo <haiven_123@163.com>
2024-05-07 22:22:06 +0800

    feature: 优化了打包策略

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -49,6 +49,9 @@ export default ({ mode, command }: ConfigEnv): UserConfigExport => {
       // 使用路径别名时想要省略的后缀名，官方不建议将.vue文件省略后缀
       extensions: ['.ts', '.tsx', '.js', '.m.js']
     },
+    optimizeDeps: {
+      holdUntilCrawlEnd: false
+    },
     server: {
       // 是否开启 https
       port: Number(VITE_APP_PORT),
@@ -74,6 +77,7 @@ export default ({ mode, command }: ConfigEnv): UserConfigExport => {
       }
     },
     css: {
+      preprocessorMaxWorkers: true,
       preprocessorOptions: {
         less: {
           math: 'always',
