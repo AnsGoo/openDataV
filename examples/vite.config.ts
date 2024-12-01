fb6b44302c89e1b2049143a22db67c6b4a2f6887
Ansgoo <haiven_123@163.com>
2024-12-01 19:21:16 +0800

    fix: 修复了预览异常和复制异常

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -38,9 +38,6 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       // 使用路径别名时想要省略的后缀名，官方不建议将.vue文件省略后缀
       extensions: ['.ts', '.tsx', '.js', '.m.js']
     },
-    optimizeDeps: {
-      holdUntilCrawlEnd: false
-    },
     server: {
       // 是否开启 https
       port: Number(VITE_APP_PORT),
@@ -66,7 +63,6 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       }
     },
     css: {
-      preprocessorMaxWorkers: true,
       preprocessorOptions: {
         less: {
           math: 'always',
